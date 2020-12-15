89d5712b3fbd10611f78976143e25d875466be67
leon0128 <18aj099@ms.dendai.ac.jp>
2020-12-15 10:48:13 +0900

    [update] implements main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
+#include "controller.hpp"
+
 int main(int argc, char **argv)
 {
-    return 0;
+    Controller controller(argc, argv);
+    if(controller.execute())
+        return 0;
+    else
+        return 1;
 }
