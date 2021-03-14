3ce89d32c492a72ed6f6f53c49f3225722f4cd2d
leon0128 <18aj099@ms.dendai.ac.jp>
2021-03-15 01:42:03 +0900

    [update] create Scope class

--- src/scope.cpp
+++ src/scope.cpp
@@ -1,2 +1,75 @@
+#include <iostream>
+
 #include "scope.hpp"
 
+namespace SCOPE
+{
+
+Scope::Scope(Scope *parent
+    , ScopeTag tag)
+    : mParent{parent}
+    , mChildren{}
+    , mScopeTag{tag}
+    , mArr{Set{}, Set{}, Set{}, Set{}}
+{
+}
+
+Scope::~Scope()
+{
+    for(auto &&s : mChildren)
+        delete s;
+}
+
+Scope *Scope::addChild(ScopeTag tag)
+{
+    Scope *child{new Scope{this, tag}};
+    mChildren.push_back(child);
+    return child;
+}
+
+Scope *Scope::getParent() const noexcept
+{
+    return mParent;
+}
+
+bool Scope::addIdentifier(const Identifier &id
+    , NamespaceTag nTag
+    , ScopeTag sTag)
+{
+    Scope *scope{this};
+    for(;
+        scope != nullptr
+            ? scope->scopeTag() != sTag
+            : false;
+        scope = scope->getParent())
+        ;
+    
+    if(scope == nullptr)
+    {
+        std::cerr << "Scope error:\n"
+            "    what: fail to find scope.\n"
+            "    tag: " << static_cast<std::size_t>(sTag)
+            << std::endl;
+        return false;
+    }
+
+    auto &&[iter, isSuccessful]{scope->set(nTag).emplace(id)};
+    if(!isSuccessful)
+    {
+        std::cerr << "Scope error:\n"
+            "    what: fail to add identifier.\n"
+            "    key: " << id.key() << "\n"
+            "    id: " << id.id()
+            << std::endl;
+    }
+
+    return isSuccessful;
+}
+
+bool Scope::addIdentifier(const Identifier &id
+    , NamespaceTag nTag)
+{
+    return addIdentifier(id, nTag, scopeTag());
+}
+
+}
--- src/scope.hpp
+++ src/scope.hpp
@@ -1,15 +1,68 @@
 #ifndef SCOPE_HPP
 #define SCOPE_HPP
 
+#include <unordered_set>
+#include <functional>
+#include <array>
 #include <any>
+
+#include "identifier.hpp"
+
+namespace SCOPE
+{
 
 class Scope
 {
 public:
-    Scope();
+    using Set = std::unordered_set<Identifier>;
+
+    inline static constexpr const std::size_t NUM_NAMESPACE{4ull};
+    enum class ScopeTag : unsigned char;
+    enum class NamespaceTag : unsigned char;
+
+    Scope(Scope*
+        , ScopeTag);
+    ~Scope();
+    Scope(const Scope&) = delete;
+    Scope(Scope&&) = delete;
+
+    Scope *addChild(ScopeTag);
+    Scope *getParent() const noexcept;
+    bool addIdentifier(const Identifier&
+        , NamespaceTag
+        , ScopeTag);
+    bool addIdentifier(const Identifier&
+        , NamespaceTag);
+
+    ScopeTag scopeTag() const noexcept
+        {return mScopeTag;}
 
 private:
-    enum class 
+    Set &set(NamespaceTag tag)
+        {return mArr[static_cast<std::size_t>(tag)];}
+
+    Scope *mParent;
+    std::vector<Scope*> mChildren;
+    ScopeTag mScopeTag;
+    std::array<Set, NUM_NAMESPACE> mArr;
 };
+
+enum class Scope::ScopeTag : unsigned char
+{
+    FILE
+    , FUNCTION
+    , BLOCK
+    , FUNCTION_PROTOTYPE
+};
+
+enum class Scope::NamespaceTag : unsigned char
+{
+    LABEL
+    , TAG
+    , MEMBER
+    , OTHER
+};
+
+}
 
 #endif
