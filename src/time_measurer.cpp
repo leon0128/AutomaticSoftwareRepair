47d0ca3d14c8bf138ab4de21f5ad12920393658f
leon0128 <22amj18@ms.dendai.ac.jp>
2022-09-22 15:02:59 +0900

    Revert "[update] change time log format"
    This reverts commit 59d5d811b9c6319153731b579fd98a27d72c25cc.

--- src/time_measurer.cpp
+++ src/time_measurer.cpp
@@ -29,8 +29,6 @@ TimeMeasurer::TimeMeasurer()
 
 void TimeMeasurer::print()
 {
-    std::cout << "time:\n" << std::flush;
-
     // this variable is reused sim-tag and repair-tag.
     // don't forget to initialize.
     Timer::Rep total{0};
