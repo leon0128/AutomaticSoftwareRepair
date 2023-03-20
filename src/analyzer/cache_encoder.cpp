109110bc270772327f815041bf5299a3010d9d73
leon0128 <22amj18@ms.dendai.ac.jp>
2023-03-20 22:25:50 +0900

    [update] declare some function

--- src/analyzer/cache_encoder.cpp
+++ src/analyzer/cache_encoder.cpp
@@ -24,6 +24,323 @@ bool CacheEncoder::execute(const std::string &filename
     return true;
 }
 
+bool CacheEncoder::encode(std::monostate)
+{
+    return true;
+}
+
+bool CacheEncoder::encode(const std::string &str)
+{
+    mStream.write(str.data(), str.size());
+    mStream.put('\0');
+
+    return true;
+}
+
+bool CacheEncoder::encode(const TOKEN::Identifier::Seq &seq)
+{
+    if(!encode(seq.size()))
+        return false;
+    
+    for(auto &&var : seq)
+    {
+        if(!encode(var))
+            return false;
+    }
+
+    return true;
+}
+
+bool CacheEncoder::encode(const TOKEN::Identifier::Element &e)
+{
+    return encode(e.index())
+        && std::visit([&](auto &&arg){return encode(arg);}, e);
+}
+
+bool CacheEncoder::encode(const TOKEN::Identifier::Id &id)
+{
+    return encode(id.first)
+        && encode(id.second);
+}
+
+bool CacheEncoder::encode(const TOKEN::StringLiteral *sl)
+{
+    if(!addStatus(sl))
+        return true;
+
+    return encode(sl->ep)
+        && encode(sl->scs);
+}
+
+bool CacheEncoder::encode(const TOKEN::Digit *digit)
+{
+    if(!addStatus(digit))
+        return true;
+
+    return encode(digit->c);
+}
+
+bool CacheEncoder::encode(const TOKEN::IntegerConstant::Sdc_is &s)
+{
+    return encode(s.dc)
+        && encode(s.is);
+}
+
+bool CacheEncoder::encode(const TOKEN::IntegerConstant::Soc_is &s)
+{
+    return encode(s.oc)
+        && encode(s.is);
+}
+
+bool CacheEncoder::encode(const TOKEN::IntegerConstant::Shc_is &s)
+{
+    return encode(s.hc)
+        && encode(s.is);
+}
+
+bool CacheEncoder::encode(const TOKEN::EnumerationConstant *ec)
+{
+    if(!addStatus(ec))
+        return true;
+
+    return encode(ec->i);
+}
+
+bool CacheEncoder::encode(const TOKEN::CharacterConstant *cc)
+{
+    if(!addStatus(cc))
+        return true;
+
+    return encode(cc->tag)
+        && encode(cc->ccs);
+}
+
+bool CacheEncoder::encode(const TOKEN::Nondigit *n)
+{
+    if(!addStatus(n))
+        return true;
+
+    return encode(n->c);
+}
+
+bool CacheEncoder::encode(const TOKEN::UniversalCharacterName::Su_hq &s)
+{
+    return encode(s.hq);
+}
+
+bool CacheEncoder::encode(const TOKEN::UniversalCharacterName::SU_hq_hq &s)
+{
+    return encode(s.hq0)
+        && encode(s.hq1);
+}
+
+bool CacheEncoder::encode(const TOKEN::DecimalConstant *dc)
+{
+    if(!addStatus(dc))
+        return true;
+
+    if(!encode(dc->nd))
+        return false;
+
+    for(auto &&d : dc->seq)
+    {
+        if(!encode(d))
+            return false;
+    }
+
+    return true;
+}
+
+bool CacheEncoder::encode(const TOKEN::HexadecimalConstant *hc)
+{
+    if(!addStatus(hc))
+        return true;
+
+    if(!encode(hc->hp))
+        return false;
+
+    for(auto &&hd : hc->seq)
+    {
+        if(!encode(hd))
+            return false;
+    }
+
+    return true;
+}
+
+bool CacheEncoder::encode(const TOKEN::DecimalFloatingConstant::Sfc_ep_fs &s)
+{
+    return encode(s.fc)
+        && encode(s.ep)
+        && encode(s.fs);
+}
+
+bool CacheEncoder::encode(const TOKEN::DecimalFloatingConstant::Sds_ep_fs &s)
+{
+    return encode(s.ds)
+        && encode(s.ep)
+        && encode(s.fs);
+}
+
+bool CacheEncoder::encode(const TOKEN::HexadecimalFloatingConstant::Shp_hfc_bep_fs &s)
+{
+    return encode(s.hp)
+        && encode(s.hfc)
+        && encode(s.bep)
+        && encode(s.fs);
+}
+
+bool CacheEncoder::encode(const TOKEN::HexadecimalFloatingConstant::Shp_hds_bep_fs &s)
+{
+    return encode(s.hp)
+        && encode(s.hds)
+        && encode(s.bep)
+        && encode(s.fs);
+}
+
+bool CacheEncoder::encode(const TOKEN::HexQuad *hq)
+{
+    if(!addStatus(hq))
+        return true;
+
+    for(auto &&hd : hq->arr)
+    {
+        if(!encode(hd))
+            return false;
+    }
+
+    return true;
+}
+
+bool CacheEncoder::encode(const TOKEN::NonzeroDigit *nd)
+{
+    if(!addStatus(nd))
+        return true;
+
+    return encode(nd->c);
+}
+
+bool CacheEncoder::encode(const TOKEN::OctalDigit *od)
+{
+    if(!addStatus(od))
+        return true;
+
+    return encode(od->c);
+}
+
+bool CacheEncoder::encode(const TOKEN::HexadecimalDigit *hd)
+{
+    if(!addStatus(hd))
+        return true;
+
+    return encode(hd->c);
+}
+
+bool CacheEncoder::encode(const TOKEN::FractionalConstant::Sds_ds &s)
+{
+    return encode(s.ds0)
+        && encode(s.ds1);
+}
+
+bool CacheEncoder::encode(const TOKEN::FractionalConstant::Sds &s)
+{
+    return encode(s.ds);
+}
+
+bool CacheEncoder::encode(const TOKEN::ExponentPart *ep)
+{
+    if(!addStatus(ep))
+        return true;
+
+    return encode(ep->s)
+        && encode(ep->ds);
+}
+
+bool CacheEncoder::encode(const TOKEN::HexadecimalFractionalConstant::Shds_hds &s)
+{
+    return encode(s.hds0)
+        && encode(s.hds1);
+}
+
+bool CacheEncoder::encode(const TOKEN::HexadecimalFractionalConstant::Shds &s)
+{
+    return encode(s.hds);
+}
+
+bool CacheEncoder::encode(const TOKEN::BinaryExponentPart *bep)
+{
+    if(!addStatus(bep))
+        return true;
+
+    return encode(bep->s)
+        && encode(bep->ds);
+}
+
+bool CacheEncoder::encode(const TOKEN::SimpleEscapeSequence *ses)
+{
+    if(!addStatus(ses))
+        return true;
+
+    return encode(ses->c);
+}
+
+bool CacheEncoder::encode(const TOKEN::IntegerSuffix::Sus_ls &s)
+{
+    return encode(s.us)
+        && encode(s.ls);
+}
+
+bool CacheEncoder::encode(const TOKEN::IntegerSuffix::Sus_lls &s)
+{
+    return encode(s.us)
+        && encode(s.lls);
+}
+
+bool CacheEncoder::encode(const TOKEN::IntegerSuffix::Sls_us &s)
+{
+    return encode(s.ls)
+        && encode(s.us);
+}
+
+bool CacheEncoder::encode(const TOKEN::IntegerSuffix::Slls_us &s)
+{
+    return encode(s.lls)
+        && encode(s.us);
+}
+
+bool CacheEncoder::encode(const TOKEN::FunctionDefinition *fd)
+{
+    if(!addStatus(fd))
+        return true;
+
+    return encode(fd->ds)
+        && encode(fd->d)
+        && encode(fd->dl)
+        && encode(fd->cs)
+        && encode(fd->scopeId)
+        && encode(fd->statementId);
+}
+
+bool CacheEncoder::encode(const TOKEN::Declaration *d)
+{
+    if(!addStatus(d))
+        return true;
+
+    return std::visit([&](auto &&arg){return encode(arg);}, d->var)
+        && encode(d->statementId);
+}
+
+bool CacheEncoder::encode(const TOKEN::Declaration::Sds_idl &s)
+{
+    return encode(s.ds)
+        && encode(s.idl);
+}
+
+bool CacheEncoder::encode(const TOKEN::Declaration::Ssad &s)
+{
+    return encode(s.sad);
+}
+
 bool CacheEncoder::align(std::stringstream::pos_type alignmentSize)
 {
     static const char padding[0xff]{'\0'};
--- src/analyzer/cache_encoder.hpp
+++ src/analyzer/cache_encoder.hpp
@@ -36,18 +36,74 @@ private:
     template<class T>
     auto encode(const T *t)
         -> decltype(t->seq, bool{});
+    // if T has only member tag(T::Tag),
+    //  this function should be used to encode.
+    // this function executes encode function to tag.
+    template<class T>
+    auto encode(const T *t)
+        -> decltype(t->tag, bool{});
 
+    // this function is used to encode integral value.
     template<class Integral>
     auto encode(Integral)
         -> std::enable_if<std::is_integral_v<Integral>, bool>::type;
     
+    // this function is used to encode enum value.
     template<class Enum>
     auto encode(Enum)
         -> std::enable_if<std::is_enum_v<Enum>, bool>::type;
 
+    // other tokens
+    bool encode(std::monostate);
+    bool encode(const std::string&);
+    bool encode(const TOKEN::Identifier::Seq&);
+    bool encode(const TOKEN::Identifier::Element&);
+    bool encode(const TOKEN::Identifier::Id&);
+    bool encode(const TOKEN::StringLiteral*);
+    bool encode(const TOKEN::Digit*);
+    bool encode(const TOKEN::IntegerConstant::Sdc_is&);
+    bool encode(const TOKEN::IntegerConstant::Soc_is&);
+    bool encode(const TOKEN::IntegerConstant::Shc_is&);
+    bool encode(const TOKEN::EnumerationConstant*);
+    bool encode(const TOKEN::CharacterConstant*);
+    bool encode(const TOKEN::Nondigit*);
+    bool encode(const TOKEN::UniversalCharacterName::Su_hq&);
+    bool encode(const TOKEN::UniversalCharacterName::SU_hq_hq&);
+    bool encode(const TOKEN::DecimalConstant*);
+    bool encode(const TOKEN::HexadecimalConstant*);
+    bool encode(const TOKEN::DecimalFloatingConstant::Sfc_ep_fs&);
+    bool encode(const TOKEN::DecimalFloatingConstant::Sds_ep_fs&);
+    bool encode(const TOKEN::HexadecimalFloatingConstant::Shp_hfc_bep_fs&);
+    bool encode(const TOKEN::HexadecimalFloatingConstant::Shp_hds_bep_fs&);
+    bool encode(const TOKEN::HexQuad*);
+    bool encode(const TOKEN::NonzeroDigit*);
+    bool encode(const TOKEN::OctalDigit*);
+    bool encode(const TOKEN::HexadecimalDigit*);
+    bool encode(const TOKEN::FractionalConstant::Sds_ds&);
+    bool encode(const TOKEN::FractionalConstant::Sds&);
+    bool encode(const TOKEN::ExponentPart*);
+    bool encode(const TOKEN::HexadecimalFractionalConstant::Shds_hds&);
+    bool encode(const TOKEN::HexadecimalFractionalConstant::Shds&);
+    bool encode(const TOKEN::BinaryExponentPart*);
+    bool encode(const TOKEN::SimpleEscapeSequence*);
+    bool encode(const TOKEN::IntegerSuffix::Sus_ls&);
+    bool encode(const TOKEN::IntegerSuffix::Sus_lls&);
+    bool encode(const TOKEN::IntegerSuffix::Sls_us&);
+    bool encode(const TOKEN::IntegerSuffix::Slls_us&);
+    bool encode(const TOKEN::FunctionDefinition*);
+    bool encode(const TOKEN::Declaration*);
+    bool encode(const TOKEN::Declaration::Sds_idl&);
+    bool encode(const TOKEN::Declaration::Ssad&);
+
     // add padding (max: 255)
     // it's should be called before arithmetic element is written.
     bool align(std::stringstream::pos_type alignmentSize);
+
+    // add pointer status to mStream.
+    // if pointer has value(i.e. not nullptr), add '\x1' to mStream and return true.
+    // otherwise(i.e. nullptr), add '\x0' to mStream and return false.
+    template<class T>
+    bool addStatus(const T*);
 
     bool output(const std::string &filename) const;
 
@@ -58,9 +114,75 @@ template<class T>
 auto CacheEncoder::encode(const T *t)
     -> decltype(t->var, bool{})
 {
-    mStream.write()
+    if(!addStatus(t))
+        return true;
 
-    return std::visit([](auto &&arg){return });
+    return encode(t->var.index())
+        && std::visit([&](auto &&arg){return encode(arg);}, t->var);
+}
+
+template<class T>
+auto CacheEncoder::encode(const T *t)
+    -> decltype(t->seq, bool{})
+{
+    if(!addStatus(t))
+        return true;
+
+    if(!encode(t->seq.size()))
+        return false;
+
+    for(auto &&e : t->seq)
+    {
+        if(!encode(e))
+            return false;
+    }
+
+    return true;
+}
+
+template<class T>
+auto CacheEncoder::encode(const T *t)
+    -> decltype(t->tag, bool{})
+{
+    if(!addStatus(t))
+        return true;
+
+    return encode(t->tag);
+}
+
+template<class Integral>
+auto CacheEncoder::encode(Integral i)
+    -> std::enable_if<std::is_integral_v<Integral>, bool>::type
+{
+    if(!align(std::alignment_of_v<Integral>))
+        return false;
+
+    mStream.write(reinterpret_cast<const char*>(&i)
+        , sizeof(i));
+
+    return true;
+}
+
+template<class Enum>
+auto CacheEncoder::encode(Enum e)
+    -> std::enable_if<std::is_enum_v<Enum>, bool>::type
+{
+    return encode(static_cast<std::underlying_type_t<Enum>>(e));
+}
+
+template<class T>
+bool CacheEncoder::addStatus(const T *t)
+{
+    if(t != nullptr)
+    {
+        encode('\x1');
+        return true;
+    }
+    else
+    {
+        encode('\x0');
+        return false;
+    }
 }
 
 }
